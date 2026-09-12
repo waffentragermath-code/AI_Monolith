e55b044504a213387914441eae7b2ff8f894ed38
Norti <k4yxd0@gmail.com>
2026-09-12 17:00:20 +0300

    Base work Monolith

--- Monolith.py
+++ Monolith.py
@@ -3,7 +3,10 @@ import queue
 import os
 import subprocess
 import sounddevice as sd
+import pyttsx3
 from vosk import KaldiRecognizer, Model
+from AppOpener import open as open_app, close as close_app
+from dicts import APP_NAMES
 
 MODEL_PATH = "model"  # Папка с распакованной моделью Vosk
 SAMPLE_RATE = 16000
@@ -16,7 +19,6 @@ def callback(indata, frames, time, status):
   if status:
     print(status)
   audio_queue.put(bytes(indata))
-
 
 def listen():
   with sd.RawInputStream(
@@ -34,17 +36,66 @@ def listen():
         if text:
           return text
 
+def voice_answer(text: str):
+  engine = pyttsx3.init()
+  engine.setProperty('rate', 180)
+  engine.setProperty('volume', 1.0)
 
-if __name__ == "__main__":
+  voices = engine.getProperty('voices')
+  engine.setProperty('voice', voices[0].id)
+  
+  engine.say(text=text)
+  engine.runAndWait()
+
+def answer(text: str):
+  print(text)
+  voice_answer(text=text)
+
+def find_app(command: str) -> str:
+  command = command.lower().strip()
+  for exe, aliases in APP_NAMES.items():
+    for alias in aliases:
+      if alias in command:
+        return exe
+  return None
+
+def app_open(app_name: str): # открытие программы по голосу
+  if app_name == None:
+    answer("Не удалось распознать название программы, повторите попытку")
+    return
+  else:
+    answer(f"Запускаю {app_name}")
+  try:
+    open_app(app_name, match_closest=True, output=False)
+  except Exception as e:
+    print(f"Exception {e}")
+
+def app_close(app_name: str): # закрытие программы по голосу
+  if app_name == None:
+    answer("Не удалось распознать название программы, повторите попытку")
+    return
+  else:
+    answer(f"Закрываю {app_name}")
+  try: 
+    close_app(app_name, match_closest=True, output=False)
+  except Exception as e:
+    print(f"Exception {e}")
+
+
+def main():
   print("Listen")
   while True:
     cmd = listen()
     if 'монолит' in cmd:
       print(cmd)
-      if "открой блокнот" in cmd:
-        print("Выполняю: Открытие блокнота")
-        # На Windows
-        if os.name == 'nt':
-          subprocess.Popen(["notepad.exe"])
+      if "открой" in cmd or "запусти" in cmd or "включи" in cmd:
+        app_open(find_app(cmd))
+      if "закрой" in cmd or "выключи" in cmd:
+        app_close(find_app(cmd))
     if "стоп" in cmd or "выход" in cmd:
+      answer("Монолит завершает работу")
       break
+
+
+if __name__ == "__main__":
+  main()
